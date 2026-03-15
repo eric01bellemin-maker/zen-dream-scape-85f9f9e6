a10ae30428fa328b1a13df8bcc26867c02170cbc
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-15 01:35:19 +0100

    Update FeaturesImageSection.tsx

--- src/components/FeaturesImageSection.tsx
+++ src/components/FeaturesImageSection.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 // on utilise le nom exact avec le "cache buster" ?v=2 pour forcer le changement
-import productImage from "@/assets/product-features.png?v=1";
+import productImage from "@/assets/product-features.png?v=3";
 
 const FeaturesImageSection = () => {
   return (
