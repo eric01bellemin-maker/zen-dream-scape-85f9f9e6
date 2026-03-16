5c7b419028d456624892006c0deec554e40a707d
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-16 16:26:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import Caracteristiques from "./pages/caracteristiques";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -15,7 +15,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+         <Route path="/" element={<Index />} />
+         <Route path="/caracteristiques" element={<Caracteristiques />} /> 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
