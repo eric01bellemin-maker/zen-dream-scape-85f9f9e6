ddceb3f25154dc3ee541e08abc7a6bde9ca170cc
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-16 16:32:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Caracteristiques from "./pages/caracteristiques";
+import Caracteristiques from "./pages/Caracteristiques";
 const queryClient = new QueryClient();
 
 const App = () => (
