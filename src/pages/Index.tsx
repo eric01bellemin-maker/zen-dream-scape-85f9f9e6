47542dacd2120b4a95fa4129aaffe82df0b0f442
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-17 00:08:59 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import SecondPageSection from "../components/SecondPageSection";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import SecondPageSection from "@/components/SecondPageSection";
 import ThirdPageSection from "../components/ThirdPageSection";
-import Footer from "../components/Footer";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
@@ -10,8 +10,11 @@ const Index = () => {
       <Navbar />
       <Hero />
       <SecondPageSection />
+      {/* Ta nouvelle section avec la vidéo */}
       <ThirdPageSection /> 
       <Footer />
     </div>
   );
 };
+
+export default Index;
