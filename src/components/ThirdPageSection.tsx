a1724be1fcd4bf4824f183a77114c133f20b6660
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 11:49:07 +0100

    Update ThirdPageSection.tsx

--- src/components/ThirdPageSection.tsx
+++ src/components/ThirdPageSection.tsx
@@ -4,11 +4,11 @@ const solutions = [
     description: "Retrouvez le sommeil même en avion ou à l'hôtel.",
     image: "/voyage.jpg", 
   },
-  {
+ {
     title: "Contre l'insomnie",
     description: "Apaisez votre esprit avec des fréquences relaxantes.",
-    image: "/insomnie.jpg", // <--- C'est cette ligne qui doit correspondre parfaitement au nom de ton fichier sur GitHub
-  },
+    image: "/insomnie.jpg", // <--- Vérifie bien que c'est .jpg ici !
+  }, 
   {
     title: "Pour les enfants",
     description: "Une aide douce pour des nuits calmes et sereines.",
