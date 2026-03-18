b2982867bcd32de2a7f6768c7a528453aa48b473
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 18:47:40 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,1 @@
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import SoundsSection from "../components/Soundssection";
-import ProductSection from "../components/ProductSection";
-import ThirdPageSection from "../components/ThirdPageSection";
 
-const Index = () => {
-  return (
-    <div style={{ backgroundColor: 'black', color: 'white', minHeight: '100vh' }}>
-      <Navbar />
-      <HeroSection />
-      <SoundsSection />
-      <ProductSection />
-      <ThirdPageSection />
-    </div>
-  );
-};
-
-export default Index;
