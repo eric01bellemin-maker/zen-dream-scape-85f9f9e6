bd24a34aa6351cf8a806cb59a5ac33c537e5e1d1
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 18:47:50 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,19 @@
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import SoundsSection from "../components/SoundsSection";
+import ProductSection from "../components/ProductSection";
+import ThirdPageSection from "../components/ThirdPageSection";
 
+const Index = () => {
+  return (
+    <div style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white' }}>
+      <Navbar />
+      <HeroSection />
+      <SoundsSection />
+      <ProductSection />
+      <ThirdPageSection />
+    </div>
+  );
+};
+
+export default Index;
