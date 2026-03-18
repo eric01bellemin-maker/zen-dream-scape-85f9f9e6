f419b5930a45c8b9a5655a511f2f06535f336780
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 23:52:04 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,24 @@
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import SoundsSection from "../components/SoundsSection";
+import ProductSection from "../components/ProductSection";
+import ThirdPageSection from "../components/ThirdPageSection";
 
+const Index = () => {
+  return (
+    <main className="min-h-screen bg-white text-gray-900 w-full relative">
+      {/* 🌫️ EFFET BROUILLARD GLOBAL : Un voile blanc très léger sur tout le site */}
+      <div className="fixed inset-0 bg-white/30 backdrop-blur-[2px] pointer-events-none z-10" />
+      
+      <div className="relative z-20">
+        <Navbar />
+        <div id="hero"><HeroSection /></div>
+        <div id="sounds"><SoundsSection /></div>
+        <div id="product"><ProductSection /></div>
+        <div id="testimonials"><ThirdPageSection /></div>
+      </div>
+    </main>
+  );
+};
+
+export default Index;
