0f14314bbcb20fff0f00a4a6daac5467a0faf4ea
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 17:05:42 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,1 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import SoundsSection from "@/components/SoundsSection";
-import ProductSection from "@/components/ProductSection";
-import ThirdPageSection from "@/components/ThirdPageSection";
 
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <HeroSection />
-      <SoundsSection />
-      <ProductSection />
-      <ThirdPageSection />
-    </div>
-  );
-};
-
-export default Index;
