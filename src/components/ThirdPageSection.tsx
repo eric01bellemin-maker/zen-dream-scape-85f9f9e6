a973ec572c1ac0d3677fbd9042381a63830f0f76
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 11:39:10 +0100

    Update ThirdPageSection.tsx

--- src/components/ThirdPageSection.tsx
+++ src/components/ThirdPageSection.tsx
@@ -7,7 +7,7 @@ const solutions = [
   {
     title: "Contre l'insomnie",
     description: "Apaisez votre esprit avec des fréquences relaxantes.",
-    image: "/insomnie.jpg", // <--- Vérifie bien qu'il n'y a pas de majuscule ici
+    image: "/insomnie.jpg", // <--- C'est cette ligne qui doit correspondre parfaitement au nom de ton fichier sur GitHub
   },
   {
     title: "Pour les enfants",
