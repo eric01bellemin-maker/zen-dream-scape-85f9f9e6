4f1b12f7e7fda6d32e7d44877f979638c8aeeab4
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-18 12:51:32 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,9 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import SoundsSection from "@/components/SoundsSection"; // <-- AJOUTÉ
 import ProductSection from "@/components/ProductSection";
 import ThirdPageSection from "@/components/ThirdPageSection";
+import FAQSection from "@/components/FAQSection"; // <-- AJOUTÉ
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -9,8 +11,18 @@ const Index = () => {
     <div className="min-h-screen bg-white">
       <Navbar />
       <HeroSection />
+      
+      {/* On affiche les 4 ambiances sonores ici */}
+      <SoundsSection /> 
+      
       <ProductSection />
+      
+      {/* On affiche les solutions Voyage/Insomnie ici */}
       <ThirdPageSection /> 
+      
+      {/* On affiche la FAQ juste avant le pied de page */}
+      <FAQSection /> 
+      
       <Footer />
     </div>
   );
