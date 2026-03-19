f9dc22b48d670b88f54a4a20191987b522ed01ad
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-19 19:12:06 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,21 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import ProductDetails from "@/components/ProductDetails";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
 
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <Header />
+      <div className="pt-16"> {/* Ajoute un petit espace sous le menu */}
+        <Hero />
+        <ProductDetails />
+        <Testimonials />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
