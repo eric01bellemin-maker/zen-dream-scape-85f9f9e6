654eaa62e37f9775443be725e65aaf21000ccfdf
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-19 19:11:52 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,1 @@
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import ProductDetails from "../components/ProductDetails";
-import Testimonials from "../components/Testimonials";
-import Footer from "../components/Footer";
 
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-slate-50">
-      <Header />
-      <Hero />
-      <ProductDetails />
-      <Testimonials />
-      <Footer />
-    </div>
-  );
-};
-
-export default Index;
