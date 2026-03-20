ab4b9b6918570e3d6c3497a575d5c17f1a365b2d
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-20 03:53:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,30 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// REMPLACE BrowserRouter PAR HashRouter LIGNE CI-DESSOUS
+import { HashRouter, Routes, Route } from "react-router-dom"; 
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import VideoSession from "./pages/VideoSession";
 
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      {/* REMPLACE BrowserRouter PAR HashRouter LIGNE CI-DESSOUS */}
+      <HashRouter> 
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/VideoSession" element={<VideoSession />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
