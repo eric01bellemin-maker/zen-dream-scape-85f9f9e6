10caadc4f6bce7c423a1eb72ee0cc3c0078d56bd
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-21 05:40:15 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,11 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import VideoSection from "@/components/VideoSection";
 import ProductPresentation from "@/components/ProductPresentation";
 import SoundsSection from "@/components/SoundsSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FAQSection from "@/components/FAQSection";
-import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
-import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
@@ -16,20 +13,17 @@ const Index = () => {
       <Navbar />
       <HeroSection />
       
-      {/* La vidéo */}
-      <VideoSection />
-
-      {/* TA NOUVELLE PRÉSENTATION INTERACTIVE */}
+      {/* Présentation du produit avec le bon prix (35€) */}
       <ProductPresentation />
       
       <SoundsSection />
       <BenefitsSection />
       <TestimonialsSection />
       <FAQSection />
-      <CTASection />
+      
       <Footer />
     </div>
   );
-}; // On ferme la fonction Index
+};
 
-export default Index; // On exporte pour que le site puisse l'afficher
+export default Index;
