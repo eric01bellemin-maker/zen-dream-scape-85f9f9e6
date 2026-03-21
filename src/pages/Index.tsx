55068dfb76fd382403d4589212b489bd5e4ff08b
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-21 02:29:40 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,7 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import VideoSection from "@/components/VideoSection"; // On l'importe ici
+import VideoSection from "@/components/VideoSection";
 import ProductPresentation from "@/components/ProductPresentation";
-import ProductSheetSection from "@/components/ProductSheetSection";
 import SoundsSection from "@/components/SoundsSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
@@ -12,7 +11,7 @@ import Footer from "@/components/Footer";
 import { Link } from "react-router-dom";
 
 const Index = () => {
- return (
+  return (
     <div className="min-h-screen bg-background text-foreground">
       <Navbar />
       <HeroSection />
@@ -20,42 +19,8 @@ const Index = () => {
       {/* La vidéo */}
       <VideoSection />
 
-      {/* C'EST ICI QUE ÇA SE PASSE : ON AJOUTE LE NOUVEAU BLOC */}
+      {/* TA NOUVELLE PRÉSENTATION INTERACTIVE */}
       <ProductPresentation />
-
-      {/* On peut enlever ProductSection et ProductSheetSection car tout est dans Presentation désormais */}
-      
-      <SoundsSection />
-      <BenefitsSection />
-      <TestimonialsSection />
-      <FAQSection />
-      <CTASection />
-      import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import VideoSection from "@/components/VideoSection"; // On l'importe ici
-import ProductPresentation from "@/components/ProductPresentation";
-import ProductSheetSection from "@/components/ProductSheetSection";
-import SoundsSection from "@/components/SoundsSection";
-import BenefitsSection from "@/components/BenefitsSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
-import FAQSection from "@/components/FAQSection";
-import CTASection from "@/components/CTASection";
-import Footer from "@/components/Footer";
-import { Link } from "react-router-dom";
-
-const Index = () => {
- return (
-    <div className="min-h-screen bg-background text-foreground">
-      <Navbar />
-      <HeroSection />
-      
-      {/* La vidéo */}
-      <VideoSection />
-
-      {/* C'EST ICI QUE ÇA SE PASSE : ON AJOUTE LE NOUVEAU BLOC */}
-      <ProductPresentation />
-
-      {/* On peut enlever ProductSection et ProductSheetSection car tout est dans Presentation désormais */}
       
       <SoundsSection />
       <BenefitsSection />
@@ -64,4 +29,7 @@ const Index = () => {
       <CTASection />
       <Footer />
     </div>
-  ); //
+  );
+}; // On ferme la fonction Index
+
+export default Index; // On exporte pour que le site puisse l'afficher
