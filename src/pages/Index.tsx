e2e1098023c50a359dd73aa2d2795fdb0ccdbfbc
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-21 06:06:54 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import SoundsSection from "@/components/SoundsSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FAQSection from "@/components/FAQSection";
+import CTASection from "@/components/CTASection"; // 1. ON AJOUTE L'IMPORT ICI
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -13,13 +14,16 @@ const Index = () => {
       <Navbar />
       <HeroSection />
       
-      {/* Présentation du produit avec le bon prix (35€) */}
+      {/* Présentation technique (sans le prix) */}
       <ProductPresentation />
       
       <SoundsSection />
       <BenefitsSection />
       <TestimonialsSection />
       <FAQSection />
+      
+      {/* 2. ON RE-AJOUTE LE BLOC FINAL ICI (Celui avec le prix de 35€ et la confiance) */}
+      <CTASection />
       
       <Footer />
     </div>
