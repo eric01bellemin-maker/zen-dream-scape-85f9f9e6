7c61bdad2a53da6fa3c399d8ca809caa15d64e33
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-21 22:06:56 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,36 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-// Supprime les imports des composants qui affichent des captures d'écran
-// Importe tes vraies sections si elles existent :
-// import BenefitsSection from "@/components/BenefitsSection";
-// import SoundsSection from "@/components/SoundsSection";
-// ...
+import BenefitsSection from "@/components/BenefitsSection";
+import ProductInAction from "@/components/ProductInAction";
+import SoundAmbiances from "@/components/SoundAmbiances"; // Vérifie bien le nom du fichier
+import TestimonialsSection from "@/components/TestimonialsSection";
+import FAQSection from "@/components/FAQSection";
 import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <main className="min-h-screen bg-[#f0f9ff]">
+    <main className="min-h-screen bg-white">
       <Navbar />
       <HeroSection />
       
-      {/* Affiche ici tes vraies sections de contenu, pas des images */}
-      {/* <BenefitsSection /> */}
-      {/* <SoundsSection /> */}
-      {/* ... */}
+      {/* C'est ici qu'on donne les "noms" (ID) pour que les boutons fonctionnent */}
+      <div id="produit">
+        <BenefitsSection />
+        <ProductInAction />
+      </div>
       
+      <div id="sons">
+        <SoundAmbiances />
+      </div>
+
+      <div id="temoignages">
+        <TestimonialsSection />
+      </div>
+
+      <div id="faq">
+        <FAQSection />
+      </div>
+
       <Footer />
     </main>
   );
