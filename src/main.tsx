13faf8f58c9989ac36cf279fbf76269faa6d9495
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-23 00:01:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-// REMPLACER :
-import { BrowserRouter } from 'react-router-dom';
-// PAR :
-import { HashRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter as Router } from "react-router-dom"; // On utilise l'alias Router pour plus de clarté
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+    </Router>
+  </React.StrictMode>,
+)
