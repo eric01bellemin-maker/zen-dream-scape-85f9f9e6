c7658ad191d5aef66cd43768705a6183d27f8384
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-23 02:25:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import SoundAmbiances from "./components/SoundAmbiances";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
